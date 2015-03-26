8f761d00ac1de4abf2a4bd3d91742acb70965dd3
JulioMansilla <mansilla.jb@gmail.com>
2015-03-26 11:15:00 -0400

    Correct dialog, add descriptive placheholders

--- scripts/madlib.js
+++ scripts/madlib.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-	$("#boomMOTHAFUCKAAAA").click(function(){
-		alert("BONNER!");
+
+	$("#madlib-btn").click(function(){
+		$("#call-to-action").fadeOut(800);
+
+		var verb = $("#verb").val();
+		var noun = $("#noun").val();
+		var adverb = $("#adverb").val();
+		var exclamation = $("#exclamation").val();
+		var madlib = "<strong>" + exclamation + " </strong> he said, as he <strong>" + adverb + "</strong> jumped on to his <strong>" + noun + "</strong> and <strong>" + verb + "</strong> into the sunset.";		
+		$("#call-to-action").fadeIn(800);
+		$("#call-to-action").html(madlib);
+
 	});
 });
