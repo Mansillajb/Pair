154458a2d7ce86f39deb14a8f1bbf4d2e82d7de5
JulioMansilla <mansilla.jb@gmail.com>
2015-04-23 19:24:32 -0400

    Increase the fun factor of adlib

--- scripts/madlib.js
+++ scripts/madlib.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
 
 	$("#madlib-btn").click(function(){
-		$("#call-to-action").fadeOut(800);
-
+		
 		var verb = $("#verb").val();
-		var noun = $("#noun").val();
+		var noun = /*$("#noun").val();*/ "rainbowRetractorBeam";
 		var adverb = $("#adverb").val();
 		var exclamation = $("#exclamation").val();
 		var madlib = "<strong>" + exclamation + " </strong> he said, as he <strong>" + adverb + "</strong> jumped on to his <strong>" + noun + "</strong> and <strong>" + verb + "</strong> into the sunset.";		
+		
+
+		$("#call-to-action").hide();
+		$("#call-to-action").fadeOut(800);
+		$("#full").css('background-image','url(img/1.gif)');
+		$("#full").css('background-size', 'cover');
+		$("#call-to-action").css('color', 'white');
+		$("#full").css('color', 'white');
 		$("#call-to-action").fadeIn(800);
 		$("#call-to-action").html(madlib);
 
